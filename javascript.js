aa58c07e6b9579051347b69882367355b170d92a
cecilema <cecile.mayer@gmail.com>
2017-04-14 00:00:38 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ function main()
  $('.boutons').fadeIn(1000);
  $('.coder_raisons').hide();
  $('.coder').on('click', function (){
- $(this).next().slideToggle(400);
+ $(this).next().slideToggle(100);
  $(this).toggleClass('active');  
  });
  
