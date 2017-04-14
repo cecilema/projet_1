f387fa37663bfae29ccc7a14a0d425e48a7ba16e
cecilema <cecile.mayer@gmail.com>
2017-04-14 11:24:51 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ function main()
 { 
  $('.coder_raisons').hide();
  $('.chercher_raisons').hide();
+  $('.creer_raisons').hide();
+  $('.utile_raisons').hide();
  $('.boutons').fadeIn(5000);
  $('.coder').on('click', function (){
  $(this).next().slideToggle(600);
@@ -9,6 +11,16 @@ function main()
  });
  
  $('.chercher').on('click', function (){
+ $(this).next().slideToggle(600);
+ $(this).toggleClass('active');  
+ });
+ 
+ $('.creer').on('click', function (){
+ $(this).next().slideToggle(600);
+ $(this).toggleClass('active');  
+ });
+ 
+ $('.utile').on('click', function (){
  $(this).next().slideToggle(600);
  $(this).toggleClass('active');  
  });
